389849eb2f5585ff1cb0714b8bf67e4b14c0635f
Karimdev12345 <samira.yaqoob@gmail.com>
2024-08-31 16:12:53 +0500

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,39 @@
+#float
+k= 2.5
+print(type(k))  
+
+#typecasting
+v= int(k)
+print(type(v))
+
+#integer
+a= 10
+print(type(a))
+
+#string
+b= "Jhon likes apples"
+print(type(b))
+
+#typecasting
+#m= int(b)
+#print(type(m))
+
+#boolean
+n= True
+print(type(n))
+
+#boolean
+n= False
+print(type(n))
+
+#stringoperation
+num1= "Abdul"
+num2= "Karim"
+#string concatenation + join
+print(num1+ " " +num2)
+
+#reverse
+print(num1[::-1])
+
+print(num2.upper())
+
