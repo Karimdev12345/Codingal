47fb324b4fa9460d22f69950191adcbae411e731
Karimdev12345 <samira.yaqoob@gmail.com>
2024-09-15 16:09:10 +0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,4 @@
+ # In this assignment, I will be sharing on how to check the weather.
+url= "https://www.windy.com/-Waves-waves?waves,31.583,74.328,5"
+
+print("Above I have showed on how to check the weather from the weather app at your country")
