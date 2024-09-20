854a5ee4fa9e44aa9a56233c4078b322cc7b0f5a
Karimdev12345 <samira.yaqoob@gmail.com>
2024-09-20 15:06:02 +0500

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,6 @@
+number= float(input("Enter a number to find its square root: "))
+
+if number < 0:
+    print("Cannot calculate square root of a number lesser than 1.")
+else: 
+        print("The square root of number above 1.")
