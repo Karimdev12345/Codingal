d3663489c014e7cdb581f48bf27d4f8ba696b2e4
Karimdev12345 <samira.yaqoob@gmail.com>
2024-09-20 16:11:41 +0500

    Add files via upload

---
+++ DivisibleNumber.py
@@ -0,0 +1,9 @@
+print("Enter a number (Numerator):")
+num1= int(input())
+print("Enter a number (denominator):")
+num2= int(input())
+
+if num1%num2==0:
+    print("num1 is divisible by num2")
+else:
+    print("num1 is not divisible by num2")
