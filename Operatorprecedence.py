525c10559659c66e8d107a5cd04b1411c61548f0
Karimdev12345 <samira.yaqoob@gmail.com>
2024-09-20 16:11:58 +0500

    Add files via upload

---
+++ Operatorprecedence.py
@@ -0,0 +1,22 @@
+b= 4
+w= 5
+z= 3
+a= 8
+x= 7
+y= 7
+v=4
+z= (v+w) * x / y;
+print("Value of (v+w) * x/ y is", z)
+
+name= "Alex"
+age= 0
+if name =="Alex" or name == "John" and age >=2:
+    print("Hello! Welcome")
+else:
+    print("Goodbye!")
+
+
+
+    
+
+
