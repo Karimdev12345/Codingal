bc26e2352824aa5a164b464e0d0c2f6a3d61b671
Karimdev12345 <samira.yaqoob@gmail.com>
2024-11-05 17:11:16 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+empty_list= []
+print(empty_list)
+
+numbers= [20,40,60,80,100]
+print(numbers)
+
+triples= [20,40,60]*3
+print(triples)
+
+aList= [200,400,600,800,1000]
+aList= aList[::-1]
+print(aList,"\n")
